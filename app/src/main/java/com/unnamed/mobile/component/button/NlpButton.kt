a037f643771d99f2c698ecffbc3e8dde28d677ac
hfjxjjd123 <licbusiness21@gmail.com>
2023-11-16 18:55:21 +0900

    feat: +???

--- app/src/main/java/com/unnamed/mobile/component/button/NlpButton.kt
+++ app/src/main/java/com/unnamed/mobile/component/button/NlpButton.kt
@@ -3,13 +3,16 @@ package com.unnamed.mobile.component.button
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import com.unnamed.mobile.processor.NlpProcessor
 import com.unnamed.mobile.ui.theme.ButtonModifier
 
 @Composable
 fun NlpButton() {
+    val context = LocalContext.current
+
     Button(
-        onClick = { NlpProcessor.startListening() },
+        onClick = { NlpProcessor.startListening(context) },
         modifier = ButtonModifier
     ){
         Text(text = "음성인식")
