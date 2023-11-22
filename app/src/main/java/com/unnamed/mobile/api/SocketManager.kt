07e75cc08d20ba82150a867f1ec3aaa67bf4bb95
hfjxjjd123 <licbusiness21@gmail.com>
2023-11-22 19:38:35 +0900

    Socket Connection

--- app/src/main/java/com/unnamed/mobile/api/SocketManager.kt
+++ app/src/main/java/com/unnamed/mobile/api/SocketManager.kt
@@ -4,18 +4,27 @@ import android.net.http.HttpResponseCache.install
 import kotlinx.coroutines.*
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.io.ObjectOutputStream
 import java.io.OutputStreamWriter
 import java.net.ServerSocket
 import java.net.Socket
 
-class SocketManager {
+private val receivedQueue = mutableListOf<String>()
+
+interface ResponseListener {
+    fun onResponseReceived(response: String){
+        receivedQueue.add(response)
+    }
+}
+
+class SocketManager(private val responseListener: ResponseListener) {
+
     //TODO change init settings
     private val port = 5000
     private val destinationIP = "192.168.1.100"
     private val destinationPort = 6000
 
-    fun connect() {
+
+    fun serverMode() {
         Thread {
             val serverSocket = ServerSocket(port)
             println("Server started, waiting for connections...")
@@ -23,21 +32,13 @@ class SocketManager {
             while (true) {
                 val clientSocket = serverSocket.accept()
 
-                println("Connection established with ${clientSocket.remoteSocketAddress}")
-
-                // Handle incoming client requests
                 val reader = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
                 val writer = OutputStreamWriter(clientSocket.getOutputStream())
-
                 val request = reader.readLine()
-                println("Received from client: $request")
 
-                // Respond to the client
-                val response = "Hello from Android server!"
-                writer.write(response)
-                writer.flush()
+                //TODO Handle Request
 
-                // Close the resources for this client connection
+
                 writer.close()
                 reader.close()
                 clientSocket.close()
@@ -45,25 +46,26 @@ class SocketManager {
         }.start()
 
         // Client behavior: makes a request to another server/device
+
+    }
+
+    fun clientMode(request: String) {
         Thread {
             val clientSocket = Socket(destinationIP, destinationPort)
 
-            // Send a request to the server/device
             val writer = OutputStreamWriter(clientSocket.getOutputStream())
-            val request = "Hello from Android client!"
             writer.write(request)
             writer.flush()
 
-            // Receive response from the server/device
             val reader = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
             val response = reader.readLine()
-            println("Received from server/device: $response")
+            responseListener.onResponseReceived(response)
 
-            // Close the resources for this client connection
             writer.close()
             reader.close()
             clientSocket.close()
         }.start()
     }
+
 }
 
